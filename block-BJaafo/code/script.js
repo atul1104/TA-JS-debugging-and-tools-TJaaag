0ca7c38196e42e8c4b343cc9392236f31e6badd5
atul1104 <waghamareatul2@gmail.com>
2021-12-08 18:06:55 +0530

    assignment

--- block-BJaafo/code/script.js
+++ block-BJaafo/code/script.js
@@ -8,3 +8,14 @@ let expected = "Atul waghamare";
 if (result !== expected) {
   throw new Error(`${result} is not equal to ${expected}`);
 }
+
+function taxedAmount(amount, taxRate) {
+  return amount + amount * taxRate;
+}
+let finalAmount = taxedAmount(100, 0.2);
+
+let expected = 120;
+
+if (finalAmount !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
